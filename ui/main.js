1f7bdea3727a21e74f78aa8bf133144541e2e521
MayankGupta7 <30701743+MayankGupta7@users.noreply.github.com>
2017-08-12 17:39:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element =document.getElementById('main-text');
 
 element.innerHTML ="New Value";
+
+var image=document.getElementById('madi');
+imgage.onclick = function (){
+    image.style.marginleft = '100px';
+};
