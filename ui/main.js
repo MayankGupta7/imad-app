5fa47deb7526d1c655e71a05e7b18d8e529d4b0e
MayankGupta7 <30701743+MayankGupta7@users.noreply.github.com>
2017-08-12 17:52:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element =document.getElementById('main-text');
 
 element.innerHTML ="New Value";
 
-var image=document.getElementById('madi');
-image.onclick = function (){
-    image.style.marginleft = '100px';
+var img=document.getElementById('madi');
+img.onclick = function (){
+    img.style.marginleft = '1000px';
 };
